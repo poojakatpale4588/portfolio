33b63fb653d56a9f47ee4a1a2fb7bdbbaa53ce3a
poojakatpale4588 <70963338+poojakatpale4588@users.noreply.github.com>
2021-07-19 21:09:44 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "%3Cpoojakatpale4588%3E.github.io/precache-manifest.b79759704c8a49b08ee1137883ca17c9.js"
+  "/precache-manifest.ecdaaca757d237a8ead312fb3c1eefa4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("%3Cpoojakatpale4588%3E.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
